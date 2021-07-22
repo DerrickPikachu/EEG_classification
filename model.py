db2def8cd46651ca0444639a835b911411608b20
DerrickPikachu <derrfgh4563@gmail.com>
2021-07-22 11:38:22 -0400

    build EEG network

--- model.py
+++ model.py
@@ -26,10 +26,97 @@ class TestNetwork(nn.Module):
         return prob
 
 
-if __name__ == "__main__":
-    model = TestNetwork().to(device)
-    print(model)
+class EEGNet(nn.Module):
+    def __init__(self):
+        super(EEGNet, self).__init__()
 
-    X = torch.rand(1, 2, device=device)
-    pred = model(X)
-    print(f"model predict: {pred}")
+        self.firstConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=(1, 51),
+                stride=(1, 1),
+                padding=(0, 25),
+                bias=False,
+                device=device,
+            ),
+            nn.BatchNorm2d(
+                num_features=16,
+                eps=1e-05,
+                momentum=0.1,
+                affine=True,
+                track_running_stats=True,
+            )
+        )
+
+        self.depthwiseConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=(2, 1),
+                stride=(1, 1),
+                groups=16,
+                bias=False,
+            ),
+            nn.BatchNorm2d(
+               num_features=32,
+               eps=1e-05,
+               momentum=0.1,
+               affine=True,
+               track_running_stats=True,
+            ),
+            nn.ELU(alpha=1.0),
+            nn.AvgPool2d(kernel_size=(1, 4), stride=(1, 4), padding=0),
+            nn.Dropout(p=0.25)
+        )
+
+        self.separableConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=(1, 15),
+                stride=(1, 1),
+                padding=(0, 7),
+                bias=False,
+            ),
+            nn.BatchNorm2d(
+                num_features=32,
+                eps=1e-05,
+                momentum=0.1,
+                affine=True,
+                track_running_stats=True,
+            ),
+            nn.ELU(alpha=1.0),
+            nn.AvgPool2d(
+                kernel_size=(1, 8),
+                stride=(1, 8),
+                padding=0,
+            ),
+            nn.Dropout(p=0.25)
+        )
+
+        self.classify = nn.Sequential(
+            nn.Linear(
+                in_features=736,
+                out_features=2,
+                bias=True,
+            )
+        )
+
+    def forward(self, x):
+        firstFeature = self.firstConv(x)
+        depthFeature = self.depthwiseConv(firstFeature)
+        finalFeature = self.separableConv(depthFeature)
+        prediction = self.classify(finalFeature)
+        return prediction
+
+
+if __name__ == "__main__":
+    net = EEGNet()
+    print(net)
+    # model = TestNetwork().to(device)
+    # print(model)
+    #
+    # X = torch.rand(1, 2, device=device)
+    # pred = model(X)
+    # print(f"model predict: {pred}")
