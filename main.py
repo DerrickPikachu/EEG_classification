ef7e09e5ffaa3b2edcd287cb343326392cb5d6bc
Ubuntu <ubuntu@ec037-074.maas>
2021-07-23 12:32:21 +0000

    Seems best for this network

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ model = model.float()
 
 # Hyper parameter
 batch_size = 64
-learning_rate = 1e-2
+learning_rate = 1e-3
 epochs = 300
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 loss_function = nn.CrossEntropyLoss()
