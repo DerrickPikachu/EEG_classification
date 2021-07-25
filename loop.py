c8da533847ae85d1ccabaafec20b7a9c960bca69
DerrickPikachu <derrfgh4563@gmail.com>
2021-07-25 06:25:59 -0400

    Refactor loop to return accuracy

--- loop.py
+++ loop.py
@@ -7,12 +7,17 @@ from deviceCheck import device
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
     size = len(dataloader.dataset)
+    corrects = 0
+
     for batch, (X, y) in enumerate(dataloader):
         X = X.to(device)
         y = y.to(device)
         # Forward pass, model predict
         pred = model(X.float())
         loss = loss_fn(pred, y.long())
+
+        # Count the among of correct prediction
+        corrects += (pred.argmax(dim=1) == y).type(torch.long).sum().item()
 
         # Backward pass, parameter update
         optimizer.zero_grad()
@@ -22,6 +27,9 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         if batch % 5 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+    corrects = corrects / size
+    return corrects * 100
 
 
 def test_loop(dataloader, model, loss_fn):
@@ -44,11 +52,19 @@ def test_loop(dataloader, model, loss_fn):
     corrects /= size
     print(f"Test Error: \n Accuracy: {(100*corrects):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
+    return corrects * 100
+
 
 def epoch_loop(epochs, train_loader, test_loader, model, loss_function, optimizer):
+    trainAcs = [0] * (epochs + 1)
+    testAcs = [0] * (epochs + 1)
+
     for e in range(1, epochs + 1):
         print(f"Epoch [{e}] start:")
         print("-------------------------------")
-        train_loop(train_loader, model, loss_function, optimizer)
-        test_loop(test_loader, model, loss_function)
+        trainAcs[e] = train_loop(train_loader, model, loss_function, optimizer)
+        testAcs[e] = test_loop(test_loader, model, loss_function)
         print("-------------------------------\n")
+
+    print(trainAcs)
+    print(testAcs)
