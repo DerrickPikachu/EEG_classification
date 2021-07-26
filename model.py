9b9945417d8abde5deca5e0a76bc32c0968f04dd
DerrickPikachu <derrfgh4563@gmail.com>
2021-07-26 01:17:27 -0400

    Test Origin model

--- model.py
+++ model.py
@@ -116,6 +116,99 @@ class EEGNet(nn.Module):
         return prediction
 
 
+class EEGNetOrigin(nn.Module):
+    def __init__(self, activation):
+        super(EEGNetOrigin, self).__init__()
+
+        self.activationDict = {
+            'ReLU': nn.ReLU(),
+            'LeakyReLU': nn.LeakyReLU(),
+            'ELU': nn.ELU(),
+        }
+
+        self.firstConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=(1, 51),
+                stride=(1, 1),
+                padding=(0, 25),
+                bias=False,
+                dtype=torch.float,
+            ),
+            nn.BatchNorm2d(
+                num_features=16,
+                eps=1e-05,
+                momentum=0.1,
+                affine=True,
+                track_running_stats=True,
+            )
+        )
+
+        self.depthwiseConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=(2, 1),
+                stride=(1, 1),
+                groups=16,
+                bias=False,
+            ),
+            nn.BatchNorm2d(
+               num_features=32,
+               eps=1e-05,
+               momentum=0.1,
+               affine=True,
+               track_running_stats=True,
+            ),
+            self.activationDict[activation],
+            nn.AvgPool2d(kernel_size=(1, 4), stride=(1, 4), padding=0),
+            nn.Dropout(p=0.25)
+        )
+
+        self.separableConv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=(1, 15),
+                stride=(1, 1),
+                padding=(0, 7),
+                bias=False,
+            ),
+            nn.BatchNorm2d(
+                num_features=32,
+                eps=1e-05,
+                momentum=0.1,
+                affine=True,
+                track_running_stats=True,
+            ),
+            self.activationDict[activation],
+            nn.AvgPool2d(
+                kernel_size=(1, 8),
+                stride=(1, 8),
+                padding=0,
+            ),
+            nn.Dropout(p=0.25)
+        )
+
+        self.flatten = nn.Flatten()
+        self.classify = nn.Sequential(
+            nn.Linear(
+                in_features=736,
+                out_features=2,
+                bias=True,
+            )
+        )
+
+    def forward(self, x):
+        firstFeature = self.firstConv(x)
+        depthFeature = self.depthwiseConv(firstFeature)
+        finalFeature = self.separableConv(depthFeature)
+        flattenFeature = self.flatten(finalFeature)
+        prediction = self.classify(flattenFeature)
+        return prediction
+
+
 class DeepConvNet(nn.Module):
     def __init__(self, activation):
         super(DeepConvNet, self).__init__()
@@ -179,6 +272,67 @@ class DeepConvNet(nn.Module):
         return pred
 
 
+class DeepConvNetOrigin(nn.Module):
+    def __init__(self, activation):
+        super(DeepConvNetOrigin, self).__init__()
+
+        self.activationDict = {
+            'ReLU': nn.ReLU(),
+            'LeakyReLU': nn.LeakyReLU(),
+            'ELU': nn.ELU(),
+        }
+
+        self.doubleConv = nn.Sequential(
+            nn.Conv2d(1, 25, kernel_size=(1, 5), padding="valid"),
+            nn.Conv2d(25, 25, kernel_size=(2, 1), padding="valid"),
+            nn.BatchNorm2d(25),
+            self.activationDict[activation],
+            nn.MaxPool2d(kernel_size=(1, 2)),
+            nn.Dropout(p=0.5),
+        )
+
+        self.secondConv = nn.Sequential(
+            nn.Conv2d(25, 50, kernel_size=(1, 5), padding="valid"),
+            nn.BatchNorm2d(50),
+            self.activationDict[activation],
+            nn.MaxPool2d(kernel_size=(1, 2)),
+            nn.Dropout(p=0.5),
+        )
+
+        self.thirdConv = nn.Sequential(
+            nn.Conv2d(50, 100, kernel_size=(1, 5), stride=(1, 2), padding="valid"),
+            nn.BatchNorm2d(100),
+            self.activationDict[activation],
+            nn.MaxPool2d(kernel_size=(1, 2)),
+            nn.Dropout(p=0.5),
+        )
+
+        self.fourthConv = nn.Sequential(
+            nn.Conv2d(100, 200, kernel_size=(1, 5), padding="valid"),
+            nn.BatchNorm2d(200),
+            self.activationDict[activation],
+            nn.MaxPool2d(kernel_size=(1, 2)),
+            nn.Dropout(p=0.5),
+        )
+
+        self.flatten = nn.Flatten()
+        self.dense = nn.Sequential(
+            # nn.Linear(8600, 500),
+            # nn.ReLU(),
+            # nn.Linear(500, 2),
+            nn.Linear(8600, 2),
+        )
+
+    def forward(self, x):
+        firstFeature = self.doubleConv(x)
+        feature = self.secondConv(firstFeature)
+        feature = self.thirdConv(feature)
+        feature = self.fourthConv(feature)
+        flatten = self.flatten(feature)
+        pred = self.dense(flatten)
+        return pred
+
+
 def chooseModel(device):
     print("Choose the model you want to train:")
     print("1. EEGNet")
@@ -200,13 +354,14 @@ def chooseModel(device):
 
     # Build CNN
     if netStruct == 1:
-        model = EEGNet(activation).to(device)
+        # model = EEGNet(activation).to(device)
+        model = EEGNetOrigin(activation).to(device)
     else:
-        model = DeepConvNet(activation).to(device)
+        model = DeepConvNetOrigin(activation).to(device)
 
     return model
 
 
 if __name__ == "__main__":
-    net = EEGNet()
+    net = DeepConvNet('ReLU')
     print(net)
